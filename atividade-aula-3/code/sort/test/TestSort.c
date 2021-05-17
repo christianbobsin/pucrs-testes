dee674e95aeed2dc3e71caf9dc811977a3106e47
Christian Bobsin <christianbobsin@gmail.com>
2021-05-16 21:05:55 -0300

    Adicionando mais testes

--- atividade-aula-3/code/sort/test/TestSort.c
+++ atividade-aula-3/code/sort/test/TestSort.c
@@ -14,25 +14,108 @@ TEST_TEAR_DOWN(Sort)
 
 TEST(Sort, TestSortA)
 {
-  int ref[] = {1,2,3,4,5};
+  int ref[] = {1, 2, 3, 4, 5};
 
-  int sample[] = {2,1,5,3,4};
- 
+  int sample[] = {2, 1, 5, 3, 4};
+
   cj_sort(sample, 5);
 
-  // All of these should pass
-  //TEST_ASSERT_EQUAL(-2, foo(1,2));
-  //TEST_ASSERT_EQUAL(-1, foo(2,2));
-
-  TEST_ASSERT_EQUAL(ref[0],sample[0]);
-  TEST_ASSERT_EQUAL(ref[1],sample[1]);
-  TEST_ASSERT_EQUAL(ref[2],sample[2]);
-  TEST_ASSERT_EQUAL(ref[3],sample[3]);
-  TEST_ASSERT_EQUAL(ref[4],sample[4]);
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 5);
 }
 
 TEST(Sort, TestSortB)
 {
-  // This test will fail
-  //TEST_ASSERT_EQUAL(2, foo(1,2));
+
+  int ref[] = {1, 2, 3, 4, 5};
+
+  int sample[] = {2, 1, 5, 3, 4};
+
+  cj_sort(sample, 6);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 6);
+}
+
+TEST(Sort, TestSortC)
+{
+
+  int ref[] = {1, 2, 3, 4, 5};
+
+  int sample[] = {2, 1, 5, 3, 4};
+
+  cj_sort(sample, 4);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 4);
+}
+
+TEST(Sort, TestSortD)
+{
+
+  int ref[] = {1, 2, 3, 4, 5};
+
+  int sample[] = {2, 1, 5, 3, 4};
+
+  cj_sort(sample, 4);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 4);
+}
+
+TEST(Sort, TestSortE)
+{
+
+  int ref[] = {1, 2, 3, 4, 5};
+
+  int sample[] = {2, 1, 5, 3, 4};
+
+  cj_sort(sample, 3);
+
+  TEST_ASSERT_NOT_EQUAL(ref[4], sample[4]);
+}
+
+TEST(Sort, TestSortF)
+{
+
+  int ref[] = {1, 2, 3, 4, 5};
+
+  char sample[] = {2, 1, 5, 3, 4};
+
+  cj_sort(sample, 5);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 5);
+}
+
+TEST(Sort, TestSortG)
+{
+
+  int ref[] = {1, 2, 3, 4, 5};
+
+  int sample[] = {1, 2, 3, 4, 5};
+
+  cj_sort(sample, 5);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 5);
+}
+
+
+TEST(Sort, TestSortH)
+{
+
+  int ref[] = {1};
+
+  int sample[] = {1};
+
+  cj_sort(sample, 1);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample, 1);
+}
+
+TEST(Sort, TestSortI)
+{
+
+  int ref[] = {1,2,3,4,5};
+
+  int sample[5];
+
+  cj_sort(sample, 5);
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(ref, sample,5);
 }
--- atividade-aula-3/code/sort/test/test_runners/TestSort_Runner.c
+++ atividade-aula-3/code/sort/test/test_runners/TestSort_Runner.c
@@ -4,5 +4,12 @@
 TEST_GROUP_RUNNER(Sort)
 {
   RUN_TEST_CASE(Sort, TestSortA);
-  //RUN_TEST_CASE(Sort, TestSortB);
+  RUN_TEST_CASE(Sort, TestSortB);
+  RUN_TEST_CASE(Sort, TestSortC);
+  RUN_TEST_CASE(Sort, TestSortD);
+  RUN_TEST_CASE(Sort, TestSortE);
+  RUN_TEST_CASE(Sort, TestSortF);
+  RUN_TEST_CASE(Sort, TestSortG);
+  RUN_TEST_CASE(Sort, TestSortH);
+  RUN_TEST_CASE(Sort, TestSortI);
 }
